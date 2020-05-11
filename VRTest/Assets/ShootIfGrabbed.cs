ab792dde0ed0429da5ac3155b4919a1b8a955d14
Jean Confident Irene Niyizibyose <jean21101999@gmail.com>
2020-05-11 11:24:16 +0200

    bulletText linked with shootifgrabbed text

--- VRTest/Assets/ShootIfGrabbed.cs
+++ VRTest/Assets/ShootIfGrabbed.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootIfGrabbed : MonoBehaviour
 {
@@ -8,18 +9,24 @@ public class ShootIfGrabbed : MonoBehaviour
     private OVRGrabbable oVRGrabbable;
     public OVRInput.Button shootingButton;
 
+    public int maxNumberOfBullet = 10;
+    public Text bulletText;
+
     private void Start()
     {
         simpleShoot = GetComponent<SimpleShoot>();
         oVRGrabbable = GetComponent<OVRGrabbable>();
+        bulletText.text = maxNumberOfBullet.ToString();
     }
 
     private void Update()
     {
-        if (oVRGrabbable.isGrabbed && OVRInput.GetDown(shootingButton, oVRGrabbable.grabbedBy.GetController()))
+        if (oVRGrabbable.isGrabbed && OVRInput.GetDown(shootingButton, oVRGrabbable.grabbedBy.GetController()) && maxNumberOfBullet>0)
         {
 
             simpleShoot.TriggerShoot();
+            maxNumberOfBullet--;
+            bulletText.text = maxNumberOfBullet.ToString();
            
         }
     }
